a9572f381d0586ebf234bc7aad6d225f26bd8441
feixue <feixue@esign.cn>
2024-06-24 14:27:06 +0800

    脚本

--- pkg/server/task/server_offline_task.go
+++ pkg/server/task/server_offline_task.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	fmt.Println("任务：服务端离线任务初始化成功")
+	fmt.Println("[定时任务]服务端离线检测任务启动")
 	// 3分钟检测一次服务端是否在线， 主要用于客户端异常掉线（服务端未获取到关闭事件）/ 服务端异常掉线重启的情况
 	// TODO 查询数据库，获取服务端最后一次心跳时间是否超过3分钟，且未通知过
 	serverDao := dao.NewServerDao()
@@ -16,7 +16,7 @@ func init() {
 		for {
 			select {
 			case <-ticket:
-				fmt.Println("检测服务端是否在线")
+				fmt.Println("[定时任务]检测服务端是否在线")
 				serverDao.CheckServerOffline()
 			}
 		}
--- pkg/server/task/server_state_delete_task.go
+++ pkg/server/task/server_state_delete_task.go
@@ -8,14 +8,14 @@ import (
 
 func init() {
 	// 自动删除30天前的任务
-	fmt.Println("初始化自动删除30天前的服务记录数据任务")
+	fmt.Println("[定时任务]自动删除30天前的服务记录数据任务启动")
 
 	c := cron.New()
 
 	serverStateDao := dao.NewServerStateDao()
 	// 每天凌晨1点执行
 	c.AddFunc("0 0 1 * * *", func() {
-		fmt.Println("自动删除30天前的服务记录数据任务")
+		fmt.Println("[定时任务]开始自动删除30天前的服务记录数据任务")
 		// 删除30天前的服务记录数据
 		serverStateDao.DeleteThirtyDaysAgo()
 	})
