8bc0ec3f017c2668c8fce846bfbd70ef8a2498b9
feixue <feixue@esign.cn>
2024-06-24 14:40:31 +0800

    模板样式

--- template/default-backend/src/main.ts
+++ template/default-backend/src/main.ts
@@ -8,7 +8,7 @@ import App from './App.vue'
 import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-
+import * as bootstrap from 'bootstrap'
 const app = createApp(App)
 
 app.use(createPinia())
